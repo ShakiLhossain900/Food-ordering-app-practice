1b931f4cda9283b4fa272f15566e94701ed0b2ce
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-21 23:00:22 +0600

    adding something new

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css'
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <React.Fragment>
+            <header className={classes.header} >
+                <h1>ReactMeals</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="A table full of delicious food" />
+            </div>
+        </React.Fragment>
     );
 };
 
