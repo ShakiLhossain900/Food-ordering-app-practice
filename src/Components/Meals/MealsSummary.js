f0d02b06d9377d127e26c253e4c088953a20933f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-25 09:06:51 +0600

    adding something new

---
+++ src/Components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+    return (
+        <section>
+            <h2>Delicious Food, Delivered To You</h2>
+            <p>
+                Choose your favorite meal from our broad selection of available meals and 
+                enjoy a delicious lunch or dinner at home. 
+            </p>
+            <p>
+                All our meals cooked with high-quality ingredients, just-in-time and of course
+                experience chefs!
+            </p>
+        </section>
+    );
+};
+
+export default MealsSummary;
