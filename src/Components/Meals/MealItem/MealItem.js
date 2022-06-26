4b5d7946ba2a828e5c1137084ae569fcf1db1aa0
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-26 23:14:20 +0600

    adding

---
+++ src/Components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const MealItem = (props) => {
+  const price =`$${props.price.toFixed(2)}`;
+  return (
+    <li>
+      <div>
+        <h3>{props.name}</h3>
+        <div>{props.description}</div>
+        <div>{price}</div>
+      </div>
+      <div>
+
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
