083030cf5cb26df3f836ed9629ba88b4ec304c7e
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-06-29 23:37:42 +0600

    adding something new

--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import classes from './Modal.module.css'
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
 
-const Backdrop = props =>{};
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
 
-
-const ModalOverlay = props =>{} 
-
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
 };
 
 export default Modal;
