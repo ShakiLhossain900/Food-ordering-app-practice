19c6df23c9fda1a5d22db70432e4de6426c11f49
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-01 11:25:44 +0600

    adding something

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React,{useContext} from "react";
 import CartIcon from "../Carts/CartIcon";
 import classes from './HeaderCartButton.module.css'
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext)
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
