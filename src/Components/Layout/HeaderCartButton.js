7c68c70e1ca19f51041a5a60927d6c7dbcd14948
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-01 12:04:23 +0600

    adding something new

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -4,14 +4,21 @@ import classes from './HeaderCartButton.module.css'
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext)
+  const cartCtx = useContext(CartContext);
+
+
+  //javascript er reduce function ata 
+const numberOfCartItems = cartCtx.items.reduce((curNumber,item)=>{
+  return curNumber + item.amount;
+},0)
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
