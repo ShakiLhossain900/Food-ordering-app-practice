95620e7283ae5f72fcd9c807f43fe7a7fb8396c1
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-14 16:11:56 +0600

    adding somethig new

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 ////practicing code watching video agian and try to find out the problem 
+///there is a problem in code when i click in the cart button that button shoud work but element can't add to the header cart button
+//that i start agian recheck the code again what is the problem in the code
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
